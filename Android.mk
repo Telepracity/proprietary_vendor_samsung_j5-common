b9563f2b23b589fc9eb3cdb03e168ac527649def
Nick Verse <nikoskalomiris@gmail.com>
2016-10-04 06:25:46 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -20,6 +20,16 @@ ifeq ($(BOARD_VENDOR),samsung)
 ifneq ($(filter j5nltexx j5ltexx j53gxx j5ltedx j5lteub j5ltezt j5yltedo, $(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libHevcSwDecoder
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
